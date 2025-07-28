fb26fb0060c11af2ffc24294e34240f43e27b4de
Cewerty <88933231+Cewerty@users.noreply.github.com>
2025-07-28 22:30:03 +0500

    Created abstract and regular factory, that can create dices with different sides amount,  with template method that kit of dice with one randomization strategy

--- dice.py
+++ dice.py
@@ -1,15 +1,22 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from random import SystemRandom
+from typing import Protocol
 
-from diceStrategies import DefaultRoll, RandomStrategy, RollStrategy
+from diceStrategies import (
+    DefaultRandomStrategy,
+    DefaultRoll,
+    MathRandomStrategy,
+    PseudorandomRandomStrategy,
+    RandomStrategy,
+    RollStrategy,
+)
 
 
 @dataclass(slots=True)
 class Dice(ABC):
     _smallest_side: int
     _biggest_side: int
-    _randomizationStrategy: RandomStrategy = field(default_factory=SystemRandom)
+    _randomizationStrategy: RandomStrategy = field(default_factory=DefaultRandomStrategy)
     _rollStrategy: RollStrategy = field(default_factory=DefaultRoll)
 
     def __post_init__(self):
@@ -29,11 +36,7 @@ class Dice(ABC):
 
     @property
     def randomizationStrategy(self) -> RandomStrategy:
-        return self._rng
-
-    @randomizationStrategy.setter
-    def randomizationStrategy(self, newStrategy: RandomStrategy) -> None:
-        self._rng = newStrategy
+        return self._randomizationStrategy
 
     @property
     def rollStrategy(self):
@@ -47,4 +50,58 @@ class Dice(ABC):
         return self.roll() >= check
 
     def roll(self, modifier: int = 0) -> int:
-        return self._rollStrategy.roll(self, modifier)
+        return self.rollStrategy.roll(self, modifier)
+
+
+class AbstractDiceFabric(Protocol):
+    @abstractmethod
+    def get_randomizationStrategy(self): ...
+
+    def create_dice_set(self) -> dict:
+        return {
+            "d4": self.create_d4(),
+            "d6": self.create_d6(),
+            "d8": self.create_d8(),
+            "d10": self.create_d10(),
+            "d12": self.create_d12(),
+            "d20": self.create_d20(),
+        }
+
+    def create_d4(self):
+        return Dice(1, 4, self.get_randomizationStrategy())
+
+    def create_d6(self):
+        return Dice(1, 6, self.get_randomizationStrategy())
+
+    def create_d8(self):
+        return Dice(1, 8, self.get_randomizationStrategy())
+
+    def create_d10(self):
+        return Dice(0, 9, self.get_randomizationStrategy())
+
+    def create_d12(self):
+        return Dice(1, 12, self.get_randomizationStrategy())
+
+    def create_d20(self):
+        return Dice(1, 20, self.get_randomizationStrategy())
+
+
+class PseudorandomDiceFabric(AbstractDiceFabric):
+    def get_randomizationStrategy(self):
+        if not hasattr(self, "_randomizationStrategy"):
+            self._randomizationStrategy = PseudorandomRandomStrategy()
+        return self._randomizationStrategy
+
+
+class DefaultDiceFabric(AbstractDiceFabric):
+    def get_randomizationStrategy(self):
+        if not hasattr(self, "_randomizationStrategy"):
+            self._randomizationStrategy = DefaultRandomStrategy()
+        return self._randomizationStrategy
+
+
+class MathematicalDiceFabric(AbstractDiceFabric):
+    def get_randomizationStrategy(self):
+        if not hasattr(self, "_randomizationStrategy"):
+            self._randomizationStrategy = MathRandomStrategy()
+        return self._randomizationStrategy
