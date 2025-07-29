5db1387bdff0adc39ab89cc621ab9f8f1c3a35e7
Cewerty <88933231+Cewerty@users.noreply.github.com>
2025-07-30 03:55:44 +0500

    Refactor Dice class and move DiceFactory to the different file

--- dice.py
+++ dice.py
@@ -1,107 +1,143 @@
-from abc import ABC, abstractmethod
+"""
+Модуль содержащий логику, относящуюся к дайсам.
+
+Contains:
+- Класс дайсов (Dice)
+
+Пример использования:
+>>> dice = Dice(1, 20)
+>>> dice.roll()
+15
+>>> dice.check_success(10)
+true
+
+"""
+
 from dataclasses import dataclass, field
-from typing import Protocol
+from typing import Final
 
 from diceStrategies import (
     DefaultRandomStrategy,
     DefaultRoll,
-    MathRandomStrategy,
-    PseudorandomRandomStrategy,
     RandomStrategy,
     RollStrategy,
 )
 
 
-@dataclass(slots=True)
-class Dice(ABC):
-    _smallest_side: int
-    _biggest_side: int
-    _randomizationStrategy: RandomStrategy = field(default_factory=DefaultRandomStrategy)
-    _rollStrategy: RollStrategy = field(default_factory=DefaultRoll)
+@dataclass(slots=True, frozen=True)
+class Dice:
+    """
+    Класс для неизменяемых дайсов.
+
+    Attributes:
+        _smallest_side (int): Наименьшая стороны дайса
+        _biggest_side (int):  Наибольшая стороны дайса
+        _randomization_strategy (RandomStrategy): Стратегия генерации случайных чисел
+        _roll_strategy (RollStrategy): Стратегия броска дайса
+
+    Raises:
+        ValueError: При ситуации, когда наименьшая сторона больше наибольшей или меньше нуля
+
+    """
+
+    _smallest_side: Final[int]
+    _biggest_side: Final[int]
+    _randomization_strategy: Final[RandomStrategy] = field(default_factory=DefaultRandomStrategy)
+    _roll_strategy: Final[RollStrategy] = field(default_factory=DefaultRoll)
 
     def __post_init__(self) -> None:
-        if not (
-            (self._smallest_side >= 0)
-            and ((self._smallest_side != self._biggest_side) or (self._smallest_side > self._biggest_side))
-        ):
-            return ValueError("Not correct sides value!")
+        """
+        Инициализирует проверку на корректность сторон дайса.
+
+        Raises:
+            ValueError: возвращается в тех случах, когда наименьшая сторона больше наибольшей или меньше нуля
+
+        """
+        if self.smallest_side >= self.biggest_side or self.smallest_side < 0:
+            raise ValueError(
+                f"Incorrect sides: smallest_side ({self._smallest_side}) must be non-negative"
+                f"and less than biggest_side ({self._biggest_side})."
+            )
 
     @property
     def smallest_side(self) -> int:
+        """
+        Возвращает наименьшую сторону дайса.
+
+        Returns:
+            int: Наименьшая сторона дайса (только для чтения)
+
+        Note:
+            Значение устанавливается при создании экземпляра
+
+        """
         return self._smallest_side
 
     @property
     def biggest_side(self) -> int:
+        """
+        Возвращает наибольшую сторону дайса.
+
+        Returns:
+            int: Наибольшая стороная дайса (только для чтения)
+
+        Note:
+            Значение устанавливается при создании экземпляра
+
+        """
         return self._biggest_side
 
     @property
-    def randomizationStrategy(self) -> RandomStrategy:
-        return self._randomizationStrategy
+    def randomization_strategy(self) -> RandomStrategy:
+        """
+        Возвращает стратегию генерации случайных чисел.
+
+        Returns:
+            RandomStrategy: Стратегия генерации случайных чисел
+
+        Note:
+            Значение устанавливается при создании экземпляра
+
+        """
+        return self._randomization_strategy
 
     @property
-    def rollStrategy(self) -> RollStrategy:
-        return self._rollStrategy
+    def roll_strategy(self) -> RollStrategy:
+        """
+        Возвращает стратегию броска дайса.
 
-    @rollStrategy.setter
-    def rollStrategy(self, newRollStrategy: RollStrategy) -> None:
-        self._rollStrategy = newRollStrategy
+        Returns:
+            RollStrategy: стратегия броска дайса
 
-    def check_success(self, check: int) -> bool:
-        return self.roll() >= check
+        """
+        return self._roll_strategy
 
-    def roll(self, modifier: int = 0) -> int:
-        return self.rollStrategy.roll(self, modifier)
+    def check_success(self, check: int, inserted_roll_strategy: RollStrategy | None = None) -> bool:
+        """
+        Проверяет успешность проходения проверки при броске кубика.
 
+        Args:
+            check (int): значение проверки
+            inserted_roll_strategy (RollStrategy | None, optional): стратегия для броска
 
-class AbstractDiceFabric(Protocol):
-    @abstractmethod
-    def get_randomizationStrategy(self) -> RandomStrategy: ...
+        Returns:
+            bool: прохождение или провал проверки
 
-    def create_dice_set(self) -> dict[str: Dice]:
-        return {
-            "d4": self.create_d4(),
-            "d6": self.create_d6(),
-            "d8": self.create_d8(),
-            "d10": self.create_d10(),
-            "d12": self.create_d12(),
-            "d20": self.create_d20(),
-        }
+        """
+        return self.roll(inserted_roll_strategy=inserted_roll_strategy) >= check
 
-    def create_d4(self) -> Dice:
-        return Dice(1, 4, self.get_randomizationStrategy())
+    def roll(self, inserted_roll_strategy: RollStrategy | None = None, modifier: int = 0) -> int:
+        """
+        Выполняет бросок дайса с заданным модификатором.
 
-    def create_d6(self) -> Dice:
-        return Dice(1, 6, self.get_randomizationStrategy())
+        Args:
+            inserted_roll_strategy (RollStrategy | None, optional): стратегия для броска. Defaults to None.
+            modifier (int, optional): модификатор броска. Defaults to 0.
 
-    def create_d8(self) -> Dice:
-        return Dice(1, 8, self.get_randomizationStrategy())
+        Returns:
+            int: значение броска
 
-    def create_d10(self) -> Dice:
-        return Dice(0, 9, self.get_randomizationStrategy())
-
-    def create_d12(self) -> Dice:
-        return Dice(1, 12, self.get_randomizationStrategy())
-
-    def create_d20(self) -> Dice:
-        return Dice(1, 20, self.get_randomizationStrategy())
-
-
-class PseudorandomDiceFabric(AbstractDiceFabric):
-    def get_randomizationStrategy(self) -> PseudorandomRandomStrategy:
-        if not hasattr(self, "_randomizationStrategy"):
-            self._randomizationStrategy = PseudorandomRandomStrategy()
-        return self._randomizationStrategy
-
-
-class DefaultDiceFabric(AbstractDiceFabric):
-    def get_randomizationStrategy(self) -> DefaultRandomStrategy:
-        if not hasattr(self, "_randomizationStrategy"):
-            self._randomizationStrategy = DefaultRandomStrategy()
-        return self._randomizationStrategy
-
-
-class MathematicalDiceFabric(AbstractDiceFabric):
-    def get_randomizationStrategy(self) -> MathRandomStrategy:
-        if not hasattr(self, "_randomizationStrategy"):
-            self._randomizationStrategy = MathRandomStrategy()
-        return self._randomizationStrategy
+        """
+        if inserted_roll_strategy is None:
+            return self.roll_strategy.roll(self, modifier)
+        return inserted_roll_strategy.roll(self, modifier)
